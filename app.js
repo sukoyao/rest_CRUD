c05ec66caad6794398aaad84c64bef9aa95e26f0
Yao <sukoyao1101@gmail>
2019-04-14 14:33:51 +0800

    Add route

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-// 加上 { useNewUrlParser: true }
-mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true })
-// 設定路由
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+// 載入 restaurant model
+const Restaurant = require('./models/restaurant')
 
-mongoose.connect('mongodb://localhost/restaurant')   // 設定連線到 mongoDB
+// 設定連線到 mongoDB 加上 { useNewUrlParser: true }
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true })
 
 // mongoose 連線後透過 mongoose.connection 拿到 Connection 的物件
 const db = mongoose.connection
@@ -24,12 +21,46 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// 載入 restaurant model
-const Restaurant = require('./models/restaurant')
-
+// 設定路由
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.send('hello world')
 })
+
+// 列出全部 restaurant
+app.get('/restaurants', (req, res) => {
+  res.send('列出所有 restaurant')
+})
+
+// 新增一筆 restaurant 頁面
+app.get('/restaurants/new', (req, res) => {
+  res.send('新增 restaurant 頁面')
+})
+
+// 顯示一筆 restaurant 的詳細內容
+app.get('/restaurants/:id', (req, res) => {
+  res.send('顯示 restaurant 的詳細內容')
+})
+
+// 新增一筆  restaurant
+app.post('/restaurants', (req, res) => {
+  res.send('建立 restaurant')
+})
+
+// 修改 restaurant 頁面
+app.get('/restaurants/:id/edit', (req, res) => {
+  res.send('修改 restaurant 頁面')
+})
+
+// 修改 restaurant
+app.post('/restaurants/:id', (req, res) => {
+  res.send('修改 restaurant')
+})
+
+// 刪除 restaurant
+app.post('/restaurants/:id/delete', (req, res) => {
+  res.send('刪除 restaurant')
+})
+
 
 app.listen(3000, () => {
   console.log('App is running!')
